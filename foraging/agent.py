34c3f8017ac2c6c38f82f904a8282d9e9c957c28
semitable <almak72@gmail.com>
2018-07-12 17:13:23 +0300

    Adds function that finds the nearest food

--- foraging/agent.py
+++ foraging/agent.py
@@ -1,3 +1,4 @@
+import numpy as np
 _MAX_INT = 999999
 
 class Agent:
@@ -8,3 +9,9 @@ class Agent:
 
 	def step(self, obs):
 		raise NotImplemented("You must implement an agent")
+
+	def _closest_food(self, obs):
+		x, y = self.position
+		r, c = np.nonzero(obs.field)
+		min_idx = ((r - x) ** 2 + (c - y) ** 2).argmin()
+		return r[min_idx], c[min_idx]
