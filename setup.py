78845a7c6b5386d82ae2b26d5fed8c6395f940ee
Filippos Christianos <almak72@gmail.com>
2019-04-07 10:06:50 +0100

    Adds setup.py for pip

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lbforaging",
+    version="1.0",
+    description="Level Based Foraging Environment",
+    author="Filippos Christianos",
+    url="https://github.com/semitable/lb-foraging",
+    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        # Indicate who your project is intended for
+        "Intended Audience :: Researchers",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    install_requires=["numpy", "gym"],
+    extras_require={"test": ["pytest"]},
+)
