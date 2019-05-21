8b9fe42a817833f7332cf06e5606d077eb0ca736
Filippos Christianos <almak72@gmail.com>
2019-05-21 10:03:05 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lbforaging",
-    version="1.0.9",
+    version="1.0.10",
     description="Level Based Foraging Environment",
     author="Filippos Christianos",
     url="https://github.com/semitable/lb-foraging",
