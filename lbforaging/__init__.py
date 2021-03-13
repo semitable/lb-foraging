9de5a4c6bbcb82348a81cda2757d02cfe01943fe
Filippos Christianos <f.christianos@ed.ac.uk>
2021-03-13 13:56:59 +0000

    Makes the task significantly easier by limiting the max food level

--- lbforaging/__init__.py
+++ lbforaging/__init__.py
@@ -2,14 +2,14 @@ from gym.envs.registration import registry, register, make, spec
 from itertools import product
 
 sizes = range(5, 20)
-players = range(2, 5)
+players = range(2, 20)
 foods = range(1, 10)
 coop = [True, False]
 partial_obs = [True, False]
 
 for s, p, f, c, po in product(sizes, players, foods, coop, partial_obs):
     register(
-        id="Foraging{4}-{0}x{0}-{1}p-{2}f{3}-v0".format(s, p, f, "-coop" if c else "", "-2s" if po else ""),
+        id="Foraging{4}-{0}x{0}-{1}p-{2}f{3}-v1".format(s, p, f, "-coop" if c else "", "-2s" if po else ""),
         entry_point="lbforaging.foraging:ForagingEnv",
         kwargs={
             "players": p,
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lbforaging",
-    version="1.0.14",
+    version="1.0.15",
     description="Level Based Foraging Environment",
     author="Filippos Christianos",
     url="https://github.com/semitable/lb-foraging",
