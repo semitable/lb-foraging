b18981904cef9aeaefb3e8b93349e408897735fb
Filippos Christianos <f.christianos@ed.ac.uk>
2021-11-18 21:01:18 +0000

    Fix: adds different naming for this branch

--- lbforaging/__init__.py
+++ lbforaging/__init__.py
@@ -10,7 +10,7 @@ grid_observation = [True, False]
 for s, p, f, c, grid_obs in product(sizes, players, foods, coop, grid_observation):
     for sight in range(1, s + 1):
         register(
-            id="Foraging{5}{4}-{0}x{0}-{1}p-{2}f{3}-v2".format(s, p, f, "-coop" if c else "", "" if sight == s else f"-{sight}s", "-grid" if grid_obs else ""),
+            id="Foraging{5}{4}-fixed-{0}x{0}-{1}p-{2}f{3}-v2".format(s, p, f, "-coop" if c else "", "" if sight == s else f"-{sight}s", "-grid" if grid_obs else ""),
             entry_point="lbforaging.foraging:ForagingEnv",
             kwargs={
                 "players": p,
