a409b672bf1fab3ba1999156ccfce5ccdb58443b
Lukas Schaefer <luki.schaefer96@gmail.com>
2022-06-17 18:42:56 +0100

    simplify gym registration and separate grid observation space registry

--- lbforaging/__init__.py
+++ lbforaging/__init__.py
@@ -5,21 +5,38 @@ sizes = range(5, 20)
 players = range(2, 20)
 foods = range(1, 10)
 coop = [True, False]
-grid_observation = [True, False]
+partial_obs = [True, False]
 
-for s, p, f, c, grid_obs in product(sizes, players, foods, coop, grid_observation):
-    for sight in range(1, s + 1):
-        register(
-            id="Foraging{5}{4}-{0}x{0}-{1}p-{2}f{3}-v2".format(s, p, f, "-coop" if c else "", "" if sight == s else f"-{sight}s", "-grid" if grid_obs else ""),
-            entry_point="lbforaging.foraging:ForagingEnv",
-            kwargs={
-                "players": p,
-                "max_player_level": 3,
-                "field_size": (s, s),
-                "max_food": f,
-                "sight": sight,
-                "max_episode_steps": 50,
-                "force_coop": c,
-                "grid_observation": grid_obs,
-            },
-        )
+for s, p, f, c, po in product(sizes, players, foods, coop, partial_obs):
+    register(
+        id="Foraging{4}-{0}x{0}-{1}p-{2}f{3}-v2".format(s, p, f, "-coop" if c else "", "-2s" if po else ""),
+        entry_point="lbforaging.foraging:ForagingEnv",
+        kwargs={
+            "players": p,
+            "max_player_level": 3,
+            "field_size": (s, s),
+            "max_food": f,
+            "sight": 2 if po else s,
+            "max_episode_steps": 50,
+            "force_coop": c,
+            "grid_observation": False,
+        },
+    )
+
+def grid_registration():
+    for s, p, f, c in product(sizes, players, foods, coop):
+        for sight in [2, s]: #range(1, s + 1):
+            register(
+                id="Foraging-grid{4}-{0}x{0}-{1}p-{2}f{3}-v2".format(s, p, f, "-coop" if c else "", "" if sight == s else f"-{sight}s"),
+                entry_point="lbforaging.foraging:ForagingEnv",
+                kwargs={
+                    "players": p,
+                    "max_player_level": 3,
+                    "field_size": (s, s),
+                    "max_food": f,
+                    "sight": sight,
+                    "max_episode_steps": 50,
+                    "force_coop": c,
+                    "grid_observation": True,
+                },
+            )
