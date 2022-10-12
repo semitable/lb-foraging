405a6e97364c3763d1c2e90a70e79d68be7f4e2b
Lukas Schaefer 50028831 <lukas.schaefer@huawei.com>
2022-10-12 18:52:01 +0100

    add registration of penalty tasks

--- lbforaging/__init__.py
+++ lbforaging/__init__.py
@@ -7,10 +7,11 @@ foods = range(1, 10)
 max_food_level = [None, 1]
 coop = [True, False]
 partial_obs = [True, False]
+pens = [True, False]
 
-for s, p, f, mfl, c, po in product(sizes, players, foods, max_food_level, coop, partial_obs):
+for s, p, f, mfl, c, po, pen in product(sizes, players, foods, max_food_level, coop, partial_obs, pens):
     register(
-        id="Foraging{4}-{0}x{0}-{1}p-{2}f{3}{5}-v2".format(s, p, f, "-coop" if c else "", "-2s" if po else "", "-ind" if mfl else ""),
+        id="Foraging{4}-{0}x{0}-{1}p-{2}f{3}{5}{6}-v2".format(s, p, f, "-coop" if c else "", "-2s" if po else "", "-ind" if mfl else "", "-pen" if pen else ""),
         entry_point="lbforaging.foraging:ForagingEnv",
         kwargs={
             "players": p,
@@ -22,6 +23,7 @@ for s, p, f, mfl, c, po in product(sizes, players, foods, max_food_level, coop, 
             "max_episode_steps": 50,
             "force_coop": c,
             "grid_observation": False,
+            "penalty": 0.1 if pen else 0.0,
         },
     )
 
